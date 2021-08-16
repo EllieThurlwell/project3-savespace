6e862cb451e60f5c4a134d3604f9327da5965509
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-08-16 06:57:08 +0000

    Add for loop to append the target coordinates to target_list

--- run.py
+++ run.py
@@ -14,9 +14,9 @@ print(
 ██████╔╝██║░░██║░░╚██╔╝░░███████╗  ██████╔╝██║░░░░░██║░░██║╚█████╔╝███████╗
 ╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝  ╚═════╝░╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚══════╝"""
     +
-    "\n\nThere are enemy spacecraft all around us.\n"
+    "\n\nThere are enemy 4 spacecraft all around us.\n"
     +
-    "Shoot them with your lazer to eliminate the threat.\n\n"
+    "Shoot them with your lazer to eliminate the threat.\n"
 )
 
 # user chooses difficulty
@@ -44,62 +44,68 @@ def print_board(board_size):
 
 print_board(5)
 
+targets_list = []
+
 
 def rand_row(board_size):
+    """
+    creates a list of randomly selected row numbers
+    for the positions of the targets
+    """
     rows = []
     target_row = 0
-    for _ in range(3):
+    for _ in range(4):
         target_row = randint(0, board_size)
         rows.append(target_row)
     return rows
 
 
 def rand_col(board_size):
+    """
+    creates a list of randomly selected column
+    numbers for the positions of the targets
+    """
     cols = []
     target_col = 0
-    for _ in range(3):
+    for _ in range(4):
         target_col = randint(0, board_size)
         cols.append(target_col)
     return cols
 
 
-rand_row(5)
-rand_col(5)
+target_rows = rand_row(5)
+target_columns = rand_col(5)
+for x, y in zip(target_columns, target_rows):
+    targets_list.append((x, y))
+
+print(targets_list)
 
 
-def get_user_guess():
-    guess_col = input('Guess a column between 1 and 5: ')
-    while guess_col not in '12345':
+def get_user_guesses():
+    """
+    request user to input a row and column value
+    check if input is valid
+    """
+    user_x = input('Guess an x coordinate between 1 and 5: ')
+    while user_x not in '12345':
         print('Invalid input. Choose a number between 1 and 5: ')
-        guess_col = input('Guess a column between 1 and 5: ')
+        user_x = input('Guess a column between 1 and 5: ')
 
-    guess_row = input('Guess a row between 1 and 5: ')
-    while guess_row not in '12345':
+    user_y = input('Guess a y coordinate between 1 and 5: ')
+    while user_y not in '12345':
         print('Invalid input. Choose a number between 1 and 5: ')
-        guess_row = input('Guess a row between 1 and 5: ')
+        user_y = input('Guess a row between 1 and 5: ')
 
-    return int(guess_col) - 1, int(guess_row) - 1
+    return int(user_x) - 1, int(user_y) - 1
 
 
-get_user_guess()
-# if guess_row and guess_col
+get_user_guesses()
+
 
 # class Board:
-#     """
-#     Defines a class for board in order to
-#     access size of board and number of targets
-#     according to difficulty.
-#     With methods to add target position and
-#     player guess when needed.
-#     """
-
+#
 #     def __init__(self, size, num_targets):
 #         self.size = size
 #         self.num_targets = num_targets
-#         self.board = ["✩" for x in range(size) for y in range(size)]
 #         self.targets = []
 #         self.guesses = []
-
-#     def render_board(self):
-#         for row in self.board:
-#             print(" ".join(row))
