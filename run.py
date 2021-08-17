0d83ded5e6fd343807176a187846da5c159b3d42
Ellie Thurlwell <elliethurlwell@yahoo.co.uk>
2021-08-17 07:07:30 +0000

    Edit user guess function to contain both x and y requests

--- run.py
+++ run.py
@@ -19,30 +19,6 @@ print(
     "Shoot them with your lazer to eliminate the threat. You have 8 chances.\n"
 )
 
-# user chooses difficulty
-
-# board = []
-
-# # board size 5
-# for i in range(0, 5):
-#     board.append(['.'] * 5)
-
-
-def print_board(board_size):
-    """
-    prints the board for the user to play against
-    """
-    global board
-    board = []
-
-    for _ in range(board_size):
-        board.append(['.'] * board_size)
-
-    for row in board:
-        print(" ".join(row))
-
-
-print_board(5)
 
 targets_list = []
 
@@ -82,80 +58,102 @@ for x, y in zip(target_columns, target_rows):
 print(targets_list)
 
 
+def print_board(board_size):
+    """
+    prints the board for the user to play against
+    """
+    board = []
+
+    for _ in range(board_size):
+        board.append(['.'] * board_size)
+
+    for row in board:
+        print("  ".join(row))
+
+
+print_board(5)
+
+
 def get_user_guesses():
     """
     request user to input a row and column value
     check if input is valid
     """
-    user_x = input('Guess an x coordinate between 1 and 5: ')
-    while user_x not in '12345':
+    user_xstr = input('\nGuess an x coordinate between 1 and 5: ')
+    while user_xstr not in '12345':
         print('Invalid input. Choose a number between 1 and 5: ')
-        user_x = input('Guess a column between 1 and 5: ')
-
-    user_y = input('Guess a y coordinate between 1 and 5: ')
-    while user_y not in '12345':
-        print('Invalid input. Choose a number between 1 and 5: ')
-        user_y = input('Guess a row between 1 and 5: ')
-
-    return int(user_x) - 1, int(user_y) - 1
-
-
-# get_user_guesses()
-
-def get_eight_guesses():
-    chance = 1
-    while chance < 8:
-        print(f'Chance {chance}')
-        guess_column()
-        guess_row()
-        chance += 1
-        if chance == 8:
-            print('Out of chances!')
-
-
-user_guesses = []
-
-
-def guess_row():
-    """
-    Takes user input for y coordinate value and
-    appends it to a list of y values, taking into
-    account zero-based indexing
-    """
-    guess_rows = []
-    user_ystr = input('Guess a y coordinate between 1 and 5: ')
-    user_y = int(user_ystr) - 1
-    guess_rows.append(user_y)
-    return guess_rows
-
-
-def guess_column():
-    """
-    Takes user input for x coordinate value and
-    appends it to a list of x values, taking into
-    account zero-based indexing
-    """
+        user_xstr = input('\nGuess a column between 1 and 5: ')
     guess_columns = []
-    user_xstr = input('Guess an x coordinate between 1 and 5: ')
     user_x = int(user_xstr) - 1
     guess_columns.append(user_x)
-    return guess_columns
+
+    user_ystr = input('\nGuess a y coordinate between 1 and 5: ')
+    while user_ystr not in '12345':
+        print('Invalid input. Choose a number between 1 and 5: ')
+        user_ystr = input('\nGuess a row between 1 and 5: ')
+    guess_rows = []
+    user_y = int(user_ystr) - 1
+    guess_rows.append(user_y)
+
+    user_guesses = []
+
+    for x, y in zip(guess_columns, guess_rows):
+        user_guesses.append((x, y))
+
+    print(user_guesses)
 
 
-# Appends user x and y guesses into a list of coordinate tuples
-guess_x = guess_column()
-guess_y = guess_row()
-for x, y in zip(guess_x, guess_y):
-    user_guesses.append((x, y))
+get_user_guesses()
+
+# def get_eight_guesses():
+#     chance = 1
+#     while chance <= 8:
+#         print(f'Chance {chance}')
+#         guess_column()
+#         guess_row()
+#         chance += 1
+#         if chance > 8:
+#             print('Out of chances!')
+#             break
 
 
-get_eight_guesses()
-print(user_guesses)
+# user_guesses = []
 
-# class Board:
-#
-#     def __init__(self, size, num_targets):
-#         self.size = size
-#         self.num_targets = num_targets
-#         self.targets = []
-#         self.guesses = []
+
+# def guess_row():
+#     """
+#     Takes user input for y coordinate value and
+#     appends it to a list of y values, taking into
+#     account zero-based indexing
+#     """
+#     guess_rows = []
+#     user_ystr = input('Guess a y coordinate between 1 and 5: ')
+#     user_y = int(user_ystr) - 1
+#     guess_rows.append(user_y)
+#     return guess_rows
+
+
+# def guess_column():
+#     """
+#     Takes user input for x coordinate value and
+#     appends it to a list of x values, taking into
+#     account zero-based indexing
+#     """
+#     guess_columns = []
+#     user_xstr = input('Guess an x coordinate between 1 and 5: ')
+#     user_x = int(user_xstr) - 1
+#     guess_columns.append(user_x)
+#     return guess_columns
+
+
+# get_eight_guesses()
+
+
+# # Appends user x and y guesses into a list of coordinate tuples
+# guess_x = guess_column()
+# guess_y = guess_row()
+# for x, y in zip(guess_x, guess_y):
+#     user_guesses.append((x, y))
+
+
+# print(user_guesses)
